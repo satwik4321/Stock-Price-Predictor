9ef08d9e73b64a5889dfd0f3086a40a4758f7a3b
Landon Varner <landonv92356@gmail.com>
2024-09-20 10:45:00 -0500

    test comment

--- project/stockproject/stockproject/urls.py
+++ project/stockproject/stockproject/urls.py
@@ -12,7 +12,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls') test comment
 """
 from django.urls import path
 from myapp import views

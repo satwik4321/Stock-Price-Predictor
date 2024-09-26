a4108bbadcee2d4cc97b08549696d9b6f6e1c814
satwik4321 <94705270+satwik4321@users.noreply.github.com>
2024-09-26 00:32:44 -0500

    trial

--- project/stockproject/myapp/forms.py
+++ project/stockproject/myapp/forms.py
@@ -13,4 +13,5 @@ class StockForm(forms.Form):
     search = forms.CharField(label='Enter the ticker symbol for the stock:', max_length=100,required=False)
     choices=update_choices()
     print(choices)
-    choices = forms.ChoiceField(label='Select one of the stoks from the list:',choices=[('Select One','None Selected'),('AAPL', 'Apple'), ('GOOGL', 'Google'),('NVDA','NVIDIA'),('TSLA','Tesla'),('WBD','Warner Bros Discovery'),('AMZN','Amazon'),('INTC','Intel'),('NFLX','Netflix'),('META','Meta'),('F','Ford Motor')],required=False)
+    stock_choices=[('Select One','None Selected'),('AAPL', 'Apple'), ('GOOGL', 'Google'),('NVDA','NVIDIA'),('TSLA','Tesla'),('WBD','Warner Bros Discovery'),('AMZN','Amazon'),('INTC','Intel'),('NFLX','Netflix'),('META','Meta'),('F','Ford Motor')]
+    choices = forms.ChoiceField(label='Select one of the stoks from the list:',choices=stock_choices,required=False)

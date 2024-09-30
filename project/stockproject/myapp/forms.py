e190330d41021818e0f5f0e7684e4b60e7fa5628
Gogineni Venkata Nikhil <goginenivenkatanikhil@gmail.com>
2024-09-30 09:45:19 -0500

    GUI_changes

--- project/stockproject/myapp/forms.py
+++ project/stockproject/myapp/forms.py
@@ -1,5 +1,26 @@
+import os
 from django import forms
+from django.conf import settings
+
+def load_ticker_choices():
+    choices = [('','Select One')]
+    file_path = os.path.join(settings.BASE_DIR, 'myapp/data/stocks.csv')
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                symbol, name = line.strip().split(',')
+                choices.append((symbol, f"{name}, {symbol}"))
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return choices
 
 class StockForm(forms.Form):
+    company_with_tickers = forms.ChoiceField(
+        choices=load_ticker_choices(),
+        label="View Company Names and Tickers",
+        required=False
+    )
+    choices = forms.ChoiceField(label='Select one of the stocks from the list:',choices=[('Select One','None Selected'),('AAPL', 'Apple'), ('GOOGL', 'Google'),('NVDA','NVIDIA'),('TSLA','Tesla'),('WBD','Warner Bros Discovery'),('AMZN','Amazon'),('INTC','Intel'),('NFLX','Netflix'),('META','Meta'),('F','Ford Motor')],required=False)
     search = forms.CharField(label='Enter the ticker symbol for the stock:', max_length=100,required=False)
-    choices = forms.ChoiceField(label='Select one of the stoks from the list:',choices=[('Select One','None Selected'),('AAPL', 'Apple'), ('GOOGL', 'Google'),('NVDA','NVIDIA'),('TSLA','Tesla'),('WBD','Warner Bros Discovery'),('AMZN','Amazon'),('INTC','Intel'),('NFLX','Netflix'),('META','Meta'),('F','Ford Motor')],required=False)

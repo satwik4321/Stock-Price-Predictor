4e637823b3da20a97aabb3c99b23a34eca5505c7
Gogineni Venkata Nikhil <goginenivenkatanikhil@gmail.com>
2024-10-21 10:00:39 -0500

    search bar

--- project/stockproject/myapp/forms.py
+++ project/stockproject/myapp/forms.py
@@ -22,7 +22,8 @@ def load_ticker_choices():
 class StockForm(forms.Form):
     company_with_tickers = forms.ChoiceField(
         choices=load_ticker_choices(),
-        label="View Stocks and their ticker symbol",
+        label="View Stocks and their ticker symbols",
+        widget=forms.Select(attrs={'id': 'id_company_with_tickers'}),
         required=False
     )
     choices = forms.ChoiceField(label='Select one of the stocks from the list:',choices=[('Select One','None Selected'),('AAPL', 'Apple'), ('GOOGL', 'Google'),('NVDA','NVIDIA'),('TSLA','Tesla'),('WBD','Warner Bros Discovery'),('AMZN','Amazon'),('INTC','Intel'),('NFLX','Netflix'),('META','Meta'),('F','Ford Motor')],required=False)
